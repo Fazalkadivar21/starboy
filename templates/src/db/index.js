cf47154c29cad95518d6fc438a8318565cd5527e
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-05-31 16:10:28 +0530

    patch 3

--- templates/src/db/index.js
+++ templates/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}+${process.env.DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGO_URI}`);
   } catch (error) {
     console.error(`Can't connect to the database : ${error}`);
   }
---
+++ templates/src/middleware/someMiddleware.js
@@ -0,0 +1,1 @@
+//middleware files will be here
